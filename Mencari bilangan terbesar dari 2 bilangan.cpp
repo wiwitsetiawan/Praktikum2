2343d542ac26ceac734d2340abf5ade8ca65fc8e
wiwitsetiawan <44104635+wiwitsetiawan@users.noreply.github.com>
2019-01-30 16:07:40 +0700

    Add files via upload

---
+++ Mencari bilangan terbesar dari 2 bilangan.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+int a,b;
+            cout<<"Masukkan nilai A : ";cin>>a;
+            cout<<"Masukkan nilai B : ";cin>>b;
+            if(a<b){
+                        cout<<"B terbesar\n";
+            }else if(a>b){
+                        cout<<"A terbesar\n";
+            }else
+            cout<<"Eror";
+}
---
+++ menentukan bilangan ganjil atau genap.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int nilai,hasil;
+    cout<<"menentukan bilangan ganjil dan genap"<<endl<<"WIWIT SETIAWAN"<<endl;
+    cout<<"masukkan nilainya = ";
+    cin>>nilai;
+    hasil=nilai%2;
+    cout<<"hasilnya adalah = ";
+    if (hasil==0)
+        cout<<"genap";
+    else
+        cout<<"ganjil";
+    return 0;
+}
+
+
